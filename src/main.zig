ca7ffafbd54c1c74278376f4fbc332f731ef9c1c
max-the-mage <alxjacobs14@gmail.com>
2021-06-09 14:27:02 -0600

    fly camera

--- src/main.zig
+++ src/main.zig
@@ -11,9 +11,14 @@ const cube = @import("cube.zig");
 var camera_pos = v3.new(0.0, 0.0, 3.0);
 var camera_target = v3.zero();
 
-
 var camera_right = v3.zero();
 var camera_up: v3 = v3.zero();
+var camera_front = v3.back();
+
+var yaw: f32 = -90.0;
+var pitch: f32 = 0.0;
+
+var direction = v3.back();
 
 pub fn main() !void {
     var camera_direction = v3.norm(camera_pos.sub(camera_target));
@@ -46,6 +51,9 @@ pub fn renderTriangle() !void {
     var window: *glfw.Window = try glfw.createWindow(800, 600, "Hello World", null, null);
 
     glfw.makeContextCurrent(window);
+    glfw.setInputMode(window, .Cursor, 0x00034003);
+    _ = glfw.setCursorPosCallback(window, mouse_callback);
+    _ = glfw.setScrollCallback(window, scroll_callback);
     glfw.swapInterval(1);
 
     gl.enable(.depth_test);
@@ -53,7 +61,6 @@ pub fn renderTriangle() !void {
     // texture stuff (?)
     var crate_img = try img.Image.fromFilePath(alloc, ".\\res\\crate.png");
     defer crate_img.deinit();
-
     
     const cube_positions = [_]za.vec3{
         v3.new( 0.0,  0.0,  0.0), 
@@ -191,7 +198,7 @@ pub fn renderTriangle() !void {
     var model = za.mat4.identity();
 
     var view = za.look_at(camera_pos, camera_target, camera_up);
-    const projection = za.perspective(60.0, 800.0/600.0, 0.1, 100.0);
+    var projection = za.perspective(fov, 800.0/600.0, 0.1, 100.0);
 
     const model_loc = program.uniformLocation("model");
     const view_loc = program.uniformLocation("view");
@@ -215,8 +222,11 @@ pub fn renderTriangle() !void {
 
         gl.bindVertexArray(vao);
 
-        view = za.look_at(camera_pos, camera_pos.add(v3.back()), camera_up);
+        view = za.look_at(camera_pos, camera_pos.add(camera_front), camera_up);
         program.uniformMatrix4(view_loc, false, &.{view.data});
+
+        projection = za.perspective(fov, 800.0/600.0, 0.1, 100.0);
+        program.uniformMatrix4(proj_loc, false, &.{projection.data});
 
         for (range(cube_positions.len)) |_, j| {
             model = za.mat4.identity();
@@ -244,14 +254,60 @@ fn range(len: usize) []u0 {
 
 fn processInput(window: *glfw.Window) void {
     const camera_speed: f32 = 0.05;
-    const camera_front = v3.back();
+    // vertically locked camera front
+    const camera_vl = v3.new(camera_front.x, 0.0, camera_front.z);
     
     if (glfw.getKey(window, .W) == .Press)
-        camera_pos = camera_pos.add(camera_front.scale(camera_speed));
+        camera_pos = camera_pos.add(camera_vl.scale(camera_speed));
     if (glfw.getKey(window, .S) == .Press)
-        camera_pos = camera_pos.sub(camera_front.scale(camera_speed));
+        camera_pos = camera_pos.sub(camera_vl.scale(camera_speed));
     if (glfw.getKey(window, .A) == .Press)
         camera_pos = camera_pos.sub((v3.norm(camera_front.cross(camera_up))).scale(camera_speed));
     if (glfw.getKey(window, .D) == .Press)
         camera_pos = camera_pos.add((v3.norm(camera_front.cross(camera_up))).scale(camera_speed));
+    if (glfw.getKey(window, .LeftShift) == .Press)
+        camera_pos = camera_pos.sub(camera_up.scale(camera_speed));
+    if (glfw.getKey(window, .Space) == .Press)
+        camera_pos = camera_pos.add(camera_up.scale(camera_speed));
+}
+
+var last_x: f64 = 400.0;
+var last_y: f64 = 300.0;
+
+
+var first_mouse = true;
+fn mouse_callback(window: *glfw.Window, xpos: f64, ypos: f64) callconv(.C) void {
+    if (first_mouse) {
+        last_x = xpos;
+        last_y = ypos;
+        first_mouse = false;
+    }
+    var xoffset = xpos - last_x;
+    var yoffset = last_y - ypos;
+    last_x = xpos;
+    last_y = ypos;
+
+    const sensitivity: f64 = 0.1;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += @floatCast(f32, xoffset);
+    pitch += @floatCast(f32, yoffset);
+
+    if (pitch > 89.0) pitch = 89.0;
+    if (pitch < -89.0) pitch = -89.0;
+
+    direction.x = @cos(za.to_radians(yaw)) * @cos(za.to_radians(pitch));
+    direction.y = @sin(za.to_radians(pitch));
+    direction.z = @sin(za.to_radians(yaw)) * @cos(za.to_radians(pitch));
+
+    camera_front = direction.norm();
+}
+
+var fov: f32 = 70.0;
+
+fn scroll_callback(window: *glfw.Window, xoffset: f64, yoffset: f64) callconv(.C) void {
+    fov -= @floatCast(f32, yoffset);
+    if (fov < 1.0) fov = 1.0;
+    if (fov > 179.0) fov = 179.0;
 }
